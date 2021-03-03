edf99b0700244e94b4a48ca2e95574d319ebc8b0
nyko <nickolasx50@gmail.com>
2021-03-03 17:52:16 -0300

    avanze pagina imagenes,logos,etc.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import imagenes from './assets/imagenes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Diseños con estilo</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <div className="container .img-fluid">
+
+      <img src={imagenes.img1} class="img-thumbnail" alt="..." width="400"></img>
+      <img src={imagenes.img2} class="img-thumbnail" alt="..." width="400"></img>
     </div>
+    
+    <div className="footer">
+          <footer >
+          <a href=""><i class="fab fa-whatsapp-square fa-5x"></i></a>
+          <a href="https://www.instagram.com/maxi.tattoo_/?hl=es-la"><i class="fab fa-instagram-square fa-5x"></i></a>
+          <a href="https://www.facebook.com/maximiliano.traslavina.3"><i class="fab fa-facebook fa-5x"></i></a>
+          <p>tattoo_urbano 2021. Todos los derechos reservados a tatuarse.</p>
+        </footer>  
+    </div>
+    </div>
+        
   );
 }
 
---
+++ src/assets/imagenes.js
@@ -0,0 +1,7 @@
+import diploma_maxy from './img/diploma_maxy.png'
+import diploma_nicol from './img/diploma_nicol.png'
+
+export default {
+    "img1": diploma_maxy,
+    "img2": diploma_nicol
+}
