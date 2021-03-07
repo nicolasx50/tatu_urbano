6f9b730edf7da09a40644da24738ee369e07846d
nyko <nickolasx50@gmail.com>
2021-03-07 11:02:54 -0300

    add to carousel the images

--- src/App.js
+++ src/App.js
@@ -4,16 +4,97 @@ import imagenes from './assets/imagenes';
 function App() {
   return (
     <div >
-    <div className="container .img-fluid">
+      <div>
+      <h1>Tatu_urbano</h1>
+      <div>
+      <section id="recientes">
+        <div>
+          <h3>tatuajes recientes</h3>
+        </div>
+        <div className="carrusel">
 
-      <img src={imagenes.img1} class="img-thumbnail" alt="..." width="400"></img>
-      <img src={imagenes.img2} class="img-thumbnail" alt="..." width="400"></img>
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src={imagenes.tatu1}  width="400px" height="500px" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src={imagenes.tatu2} width="400px" height="500px" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src={imagenes.tatu3} width="400px" height="500px" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src={imagenes.tatu4} width="400px" height="500px" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src={imagenes.tatu5} width="400px" height="500px" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src={imagenes.tatu6} width="400px" height="500px" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src={imagenes.tatu7} width="400px" height="500px" alt="..." />
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+  </div>
+</div>
+      </section>
+      </div>
+    <div>
+      <div>
+    <section id="estilos">
+      <div>
+        <h3>Estilos de tatuajes</h3>
+      </div>
+    </section>
+      </div>
+    <br/>
+    <div>
+    <h4>Tradicional</h4>
+    </div>
+    <div>
+    <p>Un tatuaje cover up no es más que un diseño de tatuaje 
+      especialmente pensado para cubrir un dibujo anterior que 
+      ya no nos gusta, bien porque el tatuaje ha perdido calidad, en cuyo caso también podríamos repasarlo, bien porque representa algo que ya no queremos lucir sobre nuestra piel.</p>
+    </div>
+    <div>
+      <h4>Estilo neo tradicional</h4>
+    </div>
+    
+    <p>Realismo</p>
+    </div>
+
+    </div>
+    <div className="container .img-fluid">
+    <div className="tatuadores">
+    <h2>tatuadores</h2>
+      <img src={imagenes.img1} class="img-thumbnail" alt="..." width="400" />
+      <img src={imagenes.img2} class="img-thumbnail" alt="..." width="400" />
+    </div>
     </div>
     
     <div className="footer">
           <footer >
           <a href=""><i class="fab fa-whatsapp-square fa-5x"></i></a>
-          <a href="https://www.instagram.com/maxi.tattoo_/?hl=es-la"><i class="fab fa-instagram-square fa-5x"></i></a>
+          <a href="https://instagram.com/tattoourbano_?igshid=ldq7m9xawcdg"><i class="fab fa-instagram-square fa-5x"></i></a>
           <a href="https://www.facebook.com/maximiliano.traslavina.3"><i class="fab fa-facebook fa-5x"></i></a>
           <p>tattoo_urbano 2021. Todos los derechos reservados a tatuarse.</p>
         </footer>  
--- src/assets/imagenes.js
+++ src/assets/imagenes.js
@@ -1,7 +1,21 @@
 import diploma_maxy from './img/diploma_maxy.png'
 import diploma_nicol from './img/diploma_nicol.png'
+import serpiente from './img/serpiente.jpeg'
+import cabeza_universo from './img/cabeza_universo.jpeg'
+import calavera_marciana from './img/calavera_marciana.jpeg'
+import girasoles from './img/girasoles01.jpeg'
+import mariposas from './img/mariposas.jpeg'
+import rosa_en_los_pechos from './img/rosa_en_los_pechos.jpeg'
+import nave_espacial from './img/nave_espacial.jpeg'
 
 export default {
     "img1": diploma_maxy,
-    "img2": diploma_nicol
+    "img2": diploma_nicol,
+    "tatu1": serpiente,
+    "tatu2": cabeza_universo,
+    "tatu3": calavera_marciana,
+    "tatu4": girasoles,
+    "tatu5": mariposas,
+    "tatu6": rosa_en_los_pechos,
+    "tatu7": nave_espacial
 }
